bff60b0fd985d29d5020c46ef1eae045f65b5f4e
hilca44 <113212346+hilca44@users.noreply.github.com>
2023-12-27 22:31:03 +0100

    Add files via upload

---
+++ c-invoice12/public/lan/de.js
@@ -0,0 +1,37 @@
+var lag={
+    "iii": "ID",
+    "ord": "Auftrag",
+    "ida": "AdressID",
+    "dkv": "DatKVA",
+    "dco": "Dat AB",
+    "dde": "Dat Lief",
+    "div": "Dat Rech",
+    "dpy": "Dat bez",
+    "ite": "Auftr Pos",
+    "net": "Netto",
+    "bru": "Brutto",
+    "hid": "Ausbl",
+    "com": "Kommentr",
+    "ax_": "USt Betrg",
+    "txr": "USt",
+    "eli": "mm",
+    "yma": "IhrZeichn",
+    "pya": "",
+    "idd": "LiefAdressID",
+    "idm": "1",
+    "pji": "ProjectID",
+    mai: "e-mail",
+    pho: "Telefon",
+    mob: "mobilTel",
+    adr: "full address",
+    cop: "Kopieren",
+    add: "Hinzufuegen",
+    adr: "Adresse",
+    nex: "NeueAusg",
+    inv: "Rechnung",
+    exp: "Ausgabe"
+}
+
+module.exports={
+    lag
+}
---
+++ c-invoice12/public/lan/en.js
@@ -0,0 +1,32 @@
+var lag={
+    "iii": "ID",
+    "ord": "Order",
+    "ida": "AddressID",
+    "dkv": "DatEstim",
+    "dco": "DatConfir",
+    "dde": "DatDelive",
+    "div": "DatInvoice",
+    "dpy": "Date Payed",
+    "ite": "Order Items",
+    "net": "Netto",
+    "bru": "Brutto",
+    "hid": "Hide",
+    "com": "Comment",
+    "ax_": "Tax",
+    "txr": "TaxRate",
+    "eli": "mm",
+    "yma": "YourMark",
+    "pya": "",
+    "idd": "DelivAddrID",
+    "idm": "idm",
+    "pji": "ProjectID",
+    mai: "E-mail",
+    pho: "Phone",
+    mob: "Mobile phone",
+    adr: "Address",
+    exp: "Expense"
+}
+
+module.exports={
+    lag
+}
